9328a49c9a8f8ef5d9890c014f4b9c6eec139c35
nazhdaque <nazhdaque@email.com>
2025-04-02 10:03:16 +0300

    refactoring 1

--- js/cursor-glow.js
+++ js/cursor-glow.js
@@ -4,10 +4,12 @@ export const cursorGlow = (parentElement, childElement) => {
 	document.querySelectorAll(parentElement).forEach(tile => {
 		const child = tile.querySelector(childElement);
 		const handler = e => {
-			if (child) {
+			try {
 				const rect = child.getBoundingClientRect();
 				child.style.setProperty("--x", e.clientX - rect.left);
 				child.style.setProperty("--y", e.clientY - rect.top);
+			} catch (e) {
+				console.error(e);
 			}
 		};
 		tile.addEventListener("pointermove", throttle(handler));
--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ import { SizeSetter } from "./helpers";
 import { slideHeader } from "./slideHeader.js";
 import { cursorGlow } from "./cursor-glow.js";
 // -- html
-import gardaTiles from "../html/garda-tiles.html?raw";
-import gardaHero from "../html/garda-hero.html?raw";
+import gradientCards from "../html/gradient-cards.html?raw";
+import sectionHero from "../html/section-hero.html?raw";
 import demo from "../html/demo.html?raw";
 
 const mainContent = document.querySelector(".main-content");
-mainContent.insertAdjacentHTML("beforeend", gardaHero);
-mainContent.insertAdjacentHTML("beforeend", gardaTiles);
+mainContent.insertAdjacentHTML("beforeend", sectionHero);
+mainContent.insertAdjacentHTML("beforeend", gradientCards);
 mainContent.insertAdjacentHTML("beforeend", demo);
 
 // --
@@ -20,7 +20,7 @@ widthSetter.initWith([["w-master-1", "w-slave-1"]]);
 slideHeader(".main-header", "-slide-up");
 
 // --
-cursorGlow(".hero-tile", ".hero-tile__title");
+cursorGlow(".hero-news", ".cursor-glow");
 
 // --
 console.log("%cCoded by ✨Nazhdaque✨", "background: #222; color: chartreuse;");
