8e748deb023d9ce674fb77ddd8c142264b0d6c91
nazhdaque <nazhdaque@email.com>
2025-08-10 10:32:40 +0300

    logos

--- js/importHTML.js
+++ js/importHTML.js
@@ -1,11 +1,12 @@
 import gradientCards from "../html/gradient-cards.html?raw";
 import sectionHero from "../html/section-hero.html?raw";
 import sectionCtaProtect from "../html/section-cta-protect.html?raw";
+import sectionTest from "../html/section-test.html?raw";
 import demo from "../html/demo.html?raw";
 
 const mainContent = document.querySelector(".main-content");
+mainContent.insertAdjacentHTML("beforeend", sectionTest);
 mainContent.insertAdjacentHTML("beforeend", sectionCtaProtect);
 mainContent.insertAdjacentHTML("beforeend", sectionHero);
 mainContent.insertAdjacentHTML("beforeend", gradientCards);
-
 // mainContent.insertAdjacentHTML("beforeend", demo);
